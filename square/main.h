410a16c91a926527233763ae834b95d1e438d6d1
Garth Webb <garth.webb@gmail.com>
2017-04-05 21:20:10 -0700

    Added function prototypes for all currently defined functions

--- square/main.h
+++ square/main.h
@@ -44,6 +44,8 @@ void init_adc(void);
 void set_red(uint16_t val);
 void set_green(uint16_t val);
 void set_blue(uint16_t val);
+void handle_spi(void);
+void handle_adc(void);
 int main(void);
 
 #endif
