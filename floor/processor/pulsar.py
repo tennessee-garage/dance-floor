b9282244abb32c52e8939f71473e26fcbc5d0af7
John Brecht <john.brecht@gmail.com>
2017-06-05 16:29:53 -0700

    add multiple sources, tweak decay time

--- floor/processor/pulsar.py
+++ floor/processor/pulsar.py
@@ -52,8 +52,8 @@ class Pulsar(Base):
             #bottom-middle
             sum += px[(y+1)*8 + x][i]
 
-        #if x==0 or y==0 or x==7 or y==7:
-        #    sum /= 1.5
+        if x==0 or y==0 or x==7 or y==7:
+            sum *= 0.9
 
         return sum
 
@@ -61,29 +61,40 @@ class Pulsar(Base):
         next_pixels = []
         next_time = time.time()
 
-        if next_time - self.last_time > 1.2:
+        #reset_time could be beat driven
+        reset_time = 0.8
+
+        #whenever we hit the reset time, wipe the board and choose more sources
+        if next_time - self.last_time > reset_time:
             self.last_time = next_time
             for y in range(0, 8):
                 for x in range(0, 8):
                     next_pixels.append((0, 0, 0))
 
-            self.last_drop = randint(0, 63)
-            next_pixels[self.last_drop] = (
-                                self.max_value*random.random(),
-                                self.max_value*random.random(),
-                                self.max_value*random.random())
+            #instead of picking random count and indexes, these could be the current highest weights
+            #count: make 3-5 sources for the new blossoms
+            count = randint(3,6)
+            for i in range(0, count):
+                #index: pick a random square for the source
+                index = randint(0, 63)
+                next_pixels[index] = (
+                                    self.max_value*random.random(),
+                                    self.max_value*random.random(),
+                                    self.max_value*random.random())
 
+        #otherwise, propagate the blossoms
         else :
-            self_decay = 0.8;
-            wave_decay = 0.45;
-            max = 220
+            self_decay = 0.7;
+            wave_decay = 0.8;
+            max = 250
 
+            # for the
             for y in range(0, 8):
                 for x in range(0, 8):
                     last_pixel = self.pixels[y*8 + x];
-                    next_red = 1 * (self_decay * last_pixel[0] + wave_decay * self.neighbor_sum(x, y, 0) / 8)
-                    next_blue = 1 * (self_decay * last_pixel[1] + wave_decay * self.neighbor_sum(x, y, 1) / 8)
-                    next_green = 1 * (self_decay * last_pixel[2] + wave_decay * self.neighbor_sum(x, y, 2) / 8)
+                    next_red = self_decay * (last_pixel[0] + wave_decay * self.neighbor_sum(x, y, 0) / 8)
+                    next_blue = self_decay * (last_pixel[1] + wave_decay * self.neighbor_sum(x, y, 1) / 8)
+                    next_green = self_decay * (last_pixel[2] + wave_decay * self.neighbor_sum(x, y, 2) / 8)
                     if next_red > max:
                         next_red = max
                     if next_blue > max:
