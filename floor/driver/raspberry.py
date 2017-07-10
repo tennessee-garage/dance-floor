10fe9d4646a42b820daaa6e181a6c054dbf726fb
Garth Webb <garth.webb@gmail.com>
2017-07-10 11:20:03 -0700

    Fix some handling for the Raspberry driver.  Also add some debugging code for now.

--- floor/driver/raspberry.py
+++ floor/driver/raspberry.py
@@ -1,5 +1,6 @@
 from base import Base
 import importlib
+import sys
 
 
 class Raspberry(Base):
@@ -19,10 +20,23 @@ class Raspberry(Base):
         data = list()
         for rgb in self.leds:
             # Repack 3 10-bit values into 4 8-bit values
-            data.append(rgb[0] >> 4)
-            data.append(((rgb[0] & 0x00F) << 4) | (rgb[1] >> 6))
-            data.append(((rgb[1] & 0x3F) << 2) | ((rgb[2] & 0x300) >> 8))
-            data.append(rgb[2] & 0x0FF)
+            data.append(int(rgb[0]) >> 4)
+            data.append(((int(rgb[0]) & 0x00F) << 4) | (int(rgb[1]) >> 6))
+            data.append(((int(rgb[1]) & 0x3F) << 2) | ((int(rgb[2]) & 0x300) >> 8))
+            data.append(int(rgb[2]) & 0x0FF)
+
+        byte = 0
+        square = 0
+        for v in data:
+            if byte == 0:
+                sys.stdout.write("{:02d})".format(square))
+                square += 1
+
+            sys.stdout.write(" / {:02x}".format(v))
+
+            byte = (byte + 1) % 4
+            if byte == 0:
+                print("")
 
         self.spi.xfer(data)
 
