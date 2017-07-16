d984e3ae93ca893ae69d08e1f206d654742757dc
mike wakerly <opensource@hoho.com>
2017-07-16 14:46:31 -0400

    playlist: support `append`, `insert_next`, `go_to`, and `remove`.

--- floor/controller/playlist.py
+++ floor/controller/playlist.py
@@ -6,8 +6,6 @@ import sys
 class Playlist(object):
 
     def __init__(self, config_dir='', processor=None):
-        self.file = config_dir + "/playlist-default.json"
-
         # The index into the queue array
         self.position = -1
         # When the current processor started
@@ -17,19 +15,42 @@ class Playlist(object):
         # The current duration
         self.current_duration = None
 
+        self.queue = []
+
         # If a processor was passed in, use it, otherwise read the config file
         if processor:
-            self.data = {
-                "queue": [
-                    {
-                        "name": processor,
-                        "duration": "0",
-                    }
-                ]
-            }
+            self.append(name=processor)
         else:
-            with open(self.file) as json_data:
-                self.data = json.load(json_data)
+            filename = config_dir + "/playlist-default.json"
+            with open(filename) as json_data:
+                items = json.load(json_data).get('queue', [])
+                for item in items:
+                    self.append(
+                        item['name'],
+                        item.get('duration', 0),
+                        item.get('args')
+                    )
+
+    @staticmethod
+    def item(name, duration=0, args=None):
+        """Builds a playlist entry."""
+        return {
+            'name': name,
+            'duration': int(duration),
+            'args': args,
+        }
+
+    def append(self, name, duration=0, args=None):
+        """Add an item at the end of the playlist."""
+        self.queue.append(Playlist.item(name, duration, args))
+        return len(self.queue) - 1
+
+    def insert_next(self, name, duration=0, args=None):
+        """Add an item at the current position in the playlist."""
+        position = self.position
+        position = max(0, position)  # handle position=-1
+        self.queue.insert(self.position, Playlist.item(name, duration, args))
+        return position
 
     def next_ready(self):
         # If we just started, we're ready for a new processor
@@ -44,17 +65,49 @@ class Playlist(object):
         return False
 
     def advance(self):
-        queue = self.data["queue"]
-        queue_length = len(queue)
-        self.position = (self.position + 1) % queue_length
-        self.current = queue[self.position]
-        self.current_duration = int(self.current["duration"])
+        """Go to the next playlist item."""
+        position = (self.position + 1) % len(self.queue)
+        self.go_to(position)
+
+    def go_to(self, position):
+        """Go to a specific playlist item."""
+        queue_length = len(self.queue)
+        if position >= queue_length:
+            raise ValueError('Position {} out of range ({})'.format(position, queue_length))
+        self.position = position
+        self.current = self.queue[position]
+        self.current_duration = self.current["duration"]
         self.start_time = time()
 
+    def remove(self, position):
+        """Removes an item from the playlist. If removing the current item,
+        advances to the next item as a side-effect.
+        """
+        if position >= len(self.queue) or position < 0:
+            raise ValueError('Position {} out of range ({})'.format(position, len(self.queue)))
+        if len(self.queue) == 1:
+            raise ValueError('Cannot delete the last item in playlist.')
+
+        del self.queue[position]
+        if position < self.position:
+            # Position was behind current.
+            self.position -= 1
+        elif position == self.position:
+            # Removed current.
+            self.position -= 1
+            self.advance()
+        else:
+            # Position was ahead of current.
+            pass
+
     def get_processor_name(self):
+        if not self.current:
+            return None
         return self.current["name"]
 
     def get_processor_args(self):
+        if not self.current:
+            return None
         if "args" in self.current:
             return self.current["args"]
         else:
