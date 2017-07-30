81f217b84251aabb04aafe328bab3b488ba34be9
mike wakerly <opensource@hoho.com>
2017-07-30 16:53:14 -0400

    New processor `ChaChaCha` plus a bpm utility.

---
+++ floor/processor/chachacha.py
@@ -0,0 +1,38 @@
+import collections
+import itertools
+
+from base import Base
+from utils import clocked
+import time
+
+BLACK = (0, 0, 0)
+RED = (0xff, 0x00, 0x00)
+YELLOW = (0xff, 0xf0, 0x00)
+GREEN = (0x00, 0xff, 0x00)
+WHITE = (0xff, 0xff, 0xff)
+
+COLORS = [RED, YELLOW, GREEN, WHITE]
+
+# Pre-render the boxes.
+LINES = []
+for color in COLORS:
+    for i in xrange(2):
+        LINES.append([color, color, BLACK, BLACK, color, color, BLACK, BLACK])
+        LINES.append([color, color, BLACK, BLACK, color, color, BLACK, BLACK])
+        LINES.append([BLACK, BLACK, color, color, BLACK, BLACK, color, color])
+        LINES.append([BLACK, BLACK, color, color, BLACK, BLACK, color, color])
+
+
+class ChaChaCha(Base):
+    """Chasing boxes."""
+
+    def __init__(self, **kwargs):
+        super(ChaChaCha, self).__init__(**kwargs)
+        self.lines = collections.deque(LINES)
+
+    @clocked(frames_per_beat=2)
+    def get_next_frame(self, weights):
+        lines = list(itertools.islice(self.lines, 0, 8))
+        self.lines.rotate()
+        pixels = [pixel for line in lines for pixel in line]
+        return pixels
---
+++ floor/processor/utils.py
@@ -0,0 +1,53 @@
+import time
+
+BLANK_FRAME = [(0, 0, 0)] * 64
+
+class clocked(object):
+    """Utility decorator that interpolates a `get_next_frame` call according to bpm.
+
+    You can think of this decorator as providing a TTL cache decorator for
+    the method it decorates, the TTL being dynamically computed from
+    `processor.bpm`.
+
+    Here is an example of a trivial processor which changes colors once per
+    quarter note:
+
+        COLORS = [RED, GREEN, BLUE]
+
+        @clocked
+        def get_next_frame(self, weights):
+            self.position += 1
+            self.position %= len(COLORS)
+            color = COLORS[self.position]
+            return [color] * 64
+    """
+    def __init__(self, frames_per_beat=1):
+        """Decorator constructor.
+
+        Args
+            frames_per_beat: Period for a single frame. The default value of `1`
+                will hold `get_next_frame` for the duration of a single beat (quarter
+                note). Any numeric value is allowed; `4` means a whole 4/4 measure, where
+                `0.5` means an eighth note.
+        """
+        self.frames_per_beat = frames_per_beat
+        self.current_frame = None
+        self.last_time = None
+
+    def __call__(self, fn, *args, **kwargs):
+        def new_fn(*args, **kwargs):
+            now = time.time()
+            processor = args[0]
+            bpm = processor.bpm or 120.0
+            beats_per_second = 60.0/bpm
+            frame_period = beats_per_second / float(self.frames_per_beat)
+
+            # Too soon, return current frame.
+            if self.last_time is not None and (now - self.last_time) < frame_period:
+                return self.current_frame
+
+            # Generate a new frame and return it.
+            self.current_frame = fn(*args, **kwargs)
+            self.last_time = now
+            return self.current_frame
+        return new_fn
