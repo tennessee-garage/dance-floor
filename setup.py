beac21c5c7ee97586bf8ffb513676b67c5b2bcc8
mike wakerly <opensource@hoho.com>
2018-09-16 19:59:19 -0400

    Add setup.py and revise developer instructions.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='floor',
+    version='0.2.0',
+    description='Dance floor',
+    packages=find_packages(),
+    install_requires=[
+        'flask',
+        'pyserial',
+        'SimpleWebSocketServer',
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'freezegun',
+        'mock',
+        'nose',
+    ],
+    dependency_links=[
+        "git+https://github.com/dpallot/simple-websocket-server.git#egg=SimpleWebSocketServer"
+    ]
+)
