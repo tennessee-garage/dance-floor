41eda6689aaf6499dc818b6443818b5978e81a06
Garth Webb <garth.webb@gmail.com>
2019-09-04 07:48:36 -0700

    Fix midi mapping issue, and update a comment

--- floor/floor/processor/constants.py
+++ floor/floor/processor/constants.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Maximum value for any component color in a pixel. As currently defined,
-# colors have a 10-bit range of `[0, 1024]`
+# colors have a 10-bit range of `[0, 1023]`
 COLOR_MAXIMUM = 1023
 
 WHITE = (COLOR_MAXIMUM, COLOR_MAXIMUM, COLOR_MAXIMUM)
