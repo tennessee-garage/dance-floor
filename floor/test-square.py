12db13bde3699bc1cb34c38714da266161b740a9
Garth Webb <garth.webb@gmail.com>
2025-04-25 11:04:57 -0700

    Add additional logging, also document argument inputs better

--- floor/test-square.py
+++ floor/test-square.py
@@ -1,18 +1,41 @@
+#!/usr/bin/env python
+
+import argparse
 import sys
 
 from floor.controller import Test
 
+import logging
+LOG_FORMAT = "%(asctime)-15s | %(name)-12s (%(levelname)s): %(message)s"
+
+logger = logging.getLogger("test-square")
+
+def get_options():
+    parser = argparse.ArgumentParser(description="Probe individual squares on the floor")
+    parser.add_argument(
+        "--floor_config", dest="floor_config", default=None, help="Use this floor configuration"
+    )
+    parser.add_argument(
+        "--verbose", dest="verbose", action="store_true", help="Enable verbose logging"
+    )
+    parser.add_argument('square', type=int, help='The square to probe')
+    parser.add_argument('red', type=int, help='The red value (0-1023)')
+    parser.add_argument('green', type=int, help='The green value (0-1023)')
+    parser.add_argument('blue', type=int, help='The blue value (0-1023)')
+    return parser.parse_args()
+
 
 def main():
-    square = int(sys.argv[1])
-    red = int(sys.argv[2])
-    green = int(sys.argv[3])
-    blue = int(sys.argv[4])
+    args = get_options()
+    log_level = logging.DEBUG if args.verbose else logging.INFO
+    logging.basicConfig(level=log_level, format=LOG_FORMAT)
+
+    logger.info(f"Probing square {args.square} with color (R:{args.red}, G:{args.green}, B:{args.blue})")
 
     leds = []
     for s in range(64):
-        if s == square:
-            leds.append((red, green, blue))
+        if s == args.square:
+            leds.append((args.red, args.green, args.blue))
         else:
             leds.append((0, 0, 0))
 
